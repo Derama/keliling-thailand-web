b594f376e3902ae295c85cbb21d06691744379fa
Deva <devaa.adithya@gmail.com>
2026-06-17 18:38:21 +0700

    Add new-rental wizard

---
+++ app/rental/(panel)/rentals/new/page.tsx
@@ -0,0 +1,10 @@
+import RentalWizard from "@/components/rental/RentalWizard";
+
+export default function NewRentalPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold text-[#1B2A4A]">Sewa baru</h1>
+      <RentalWizard />
+    </div>
+  );
+}
